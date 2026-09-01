b7ea1ec173b63867a702f678c7d28e29ac3abb2e
u <fjrfhdy@gmail.com>
2026-09-01 02:41:46 -0600

    re pushing

--- check.sh
+++ check.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/home/u/.nvm/versions/node/v22.18.0/bin/node --env-file=/home/u/Downloads/xmr_store/.env /home/u/Downloads/xmr_store/cron_job/automated_checking_tx.js
+~/.nvm/versions/node/v22.18.0/bin/node --env-file=./.env ./cron_job/automated_checking_tx.js
