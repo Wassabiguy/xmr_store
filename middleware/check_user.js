cd8c0b17a41fadf15e5e28b577cb6a5a54c3f580
Wassabiguy <dr_drake@tuta.io>
2026-09-17 11:25:23 +0300

    middleware implementaed to check if the user is banned.

--- middleware/check_user.js
+++ middleware/check_user.js
@@ -2,10 +2,16 @@ import { json_reply } from "../control/dont_waste_time.js";
 import { ban } from "../model/db.js";
 
 const CheckIfUserIsbanned = async (req,res,next) => {
- const get_user = await ban.findOne({"user_name":req.body})   
+const serser = await ban.find({})
+console.log(serser)
+ const get_user = await ban.findOne({user_id:req.body.id})   
+ console.log("_-------------------")
+ console.log(req.body)
+ console.log(get_user)
  if(get_user == null){
     next()
  }else{
     json_reply(res,{"IsBanned":true})
  }
 }
+export{CheckIfUserIsbanned}
