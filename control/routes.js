5c0c81b9716d8a4c50c2f2b258096e5fac7f4778
Wassabiguy <dr_drake@tuta.io>
2026-09-17 11:29:47 +0300

    embedded the 'CheckIfUserIsbanned' middleware to the /check_out end point

--- control/routes.js
+++ control/routes.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import { Ban_user } from './admin.js'
+import { CheckIfUserIsbanned } from '../middleware/check_user.js'
 import { ConnectToDB } from '../model/db.js'
 import { add_item } from './add_item.js'
 import { mark_to_be_sent } from './admin.js'
@@ -52,11 +53,12 @@ app.post('/delete_from_cart',async (req,res) => {
     let {id,name} = data
     decrease_amount(res,data)
 })
-app.post("/check_out",async (req,res) => {
+app.post("/check_out",(req,res,next)=>{CheckIfUserIsbanned(req,res,next)},async (req,res) => {
     let data = req.body
     let {id} = data
-    CheckOut(res,data)
+    CheckOut(res,data) 
 })
+
 app.post("/submit_ticket",async(req,res)=>{
 let data = req.body
 let {id,order_id,description} =data
@@ -84,6 +86,8 @@ app.post("/ban_user",async(req,res)=>{
     let {user_name,reason} = data
     await Ban_user(req,res)
 })
+
+
 ConnectToDB().then(()=>{
     try{
     app.listen(process.env.PORT)
