95c433f96eb6d3e0a29bbb462b93ddbf326be660
Wassabiguy <dr_drake@tuta.io>
2026-09-18 19:11:59 +0300

    added a more nicer response

--- control/stocking.js
+++ control/stocking.js
@@ -3,12 +3,12 @@ import { json_reply } from "./dont_waste_time.js";
 
 
 const OutOfStock = async (res,data) => {
-const find = await items.findOne({'name':data.name,'category':data.category})
+const find = await items.findOne({'id':data.id})
 if(find == null){
     json_reply(res,'no such item in the database!')
 }else{
     await find.updateOne({'quantity_in_stock':0})
-    json_reply(res,'The item is out of stock now!')
+    json_reply(res,`The item named ${find.name}, category ${find.category} is out of stock now!`)
 }    
 }
 const ReStocking = async (res,data) => {
